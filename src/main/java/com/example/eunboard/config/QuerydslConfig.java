201c681dd6e853dbe4aae21dc0c438c7ec23302e
이강은 <rkddms123456@gmail.com>
2022-09-16 23:10:02 +0900

    set

--- src/main/java/com/example/eunboard/config/QuerydslConfig.java
+++ src/main/java/com/example/eunboard/config/QuerydslConfig.java
@@ -1,4 +1,10 @@
 package com.example.eunboard.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class QuerydslConfig {
+
+
 }
+
--- src/main/java/com/example/eunboard/domain/repository/TicketRepository.java
+++ src/main/java/com/example/eunboard/domain/repository/TicketRepository.java
@@ -3,5 +3,5 @@ package com.example.eunboard.domain.repository;
 import com.example.eunboard.domain.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface TicketRepository extends JpaRepository<Board, Long> {
 }
