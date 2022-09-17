09773c544b9356713e69464328cdfc685aea41a2
이강은 <rkddms123456@gmail.com>
2022-09-17 22:36:46 +0900

    환경세팅 완료

--- src/main/java/com/example/eunboard/domain/entity/Ticket.java
+++ src/main/java/com/example/eunboard/domain/entity/Ticket.java
@@ -1,4 +1,46 @@
 package com.example.eunboard.domain.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ticket_id")
+    private Long id;
+
+    /**  출발지 */
+    @Column(name = "start_point")
+    private String startPoint;
+
+    /** 도착지 */
+    @Column(name = "end_point")
+    private String endPoint;
+
+    /** 요일 */
+    @Column(name = "depart_day")
+    private String departDay;
+
+    /** 시간 */
+    @Column(name = "depart_time")
+    private String departTime;
+
+    /** 오픈채팅방 링크*/
+    @Column(name = "kakao_open_chat_url")
+    private String kakaoOpenChatUrl;
+
+    /** 오픈채팅방 이름 */
+    @Column(name = "kakao_open_chat_title")
+    private String kakaoOpenChatTitle;
+
+    /** 탑승 인원 */
+    @Column(name = "recruit_person")
+    private String recruitPerson;
 }
