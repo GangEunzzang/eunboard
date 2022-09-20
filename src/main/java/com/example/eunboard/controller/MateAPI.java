a2fdb1159a9a0bb9c92bd8c457282637004ef259
godseun_mac <alcy1024@gmail.com>
2022-09-21 06:06:22 +0900

    인증테스트 작성

--- src/main/java/com/example/eunboard/controller/MateAPI.java
+++ src/main/java/com/example/eunboard/controller/MateAPI.java
@@ -4,7 +4,9 @@ import com.example.eunboard.domain.dto.TicketDTO;
 import com.example.eunboard.service.TicketService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,15 @@ public class MateAPI {
     @PostMapping("/mate/api/test")
     public void createTest(TicketDTO ticketDTO) {
         ticketService.createTicket(ticketDTO);
+    }
+
+    @GetMapping("/auth/test")
+    public String authTest() {
+        return "인증 불필요 성공";
+    }
+
+    @PostMapping("/auth/test")
+    public String authTest(@AuthenticationPrincipal long memberId) {
+        return "인증 필요 성공";
     }
 }
