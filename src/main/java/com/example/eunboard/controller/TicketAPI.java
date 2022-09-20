03bb217d63fe41c976f260d8c61f0b12bd74a59f
godseun_mac <alcy1024@gmail.com>
2022-09-21 07:01:27 +0900

    티켓 컨트롤러 생성

---
+++ src/main/java/com/example/eunboard/controller/TicketAPI.java
@@ -0,0 +1,38 @@
+package com.example.eunboard.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.eunboard.domain.dto.TicketDTO;
+import com.example.eunboard.service.TicketService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class TicketAPI {
+
+  private final TicketService ticketService;
+
+  @GetMapping("/ticket")
+  public String test() {
+    return "test";
+  }
+
+  @ResponseBody
+  @PostMapping("/ticket/new")
+  public String ticketCreate(@AuthenticationPrincipal Long memberId,
+      @RequestBody TicketDTO ticketDTO) {
+
+    System.out.println("요청자 아이디 : " + memberId);
+    System.out.println("ticketDTO : " + ticketDTO);
+
+    return "티켓생성";
+  }
+}
