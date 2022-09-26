5428086e5f150a2c9634db5cbcfbfcc4b6d4924f
godseun_mac <alcy1024@gmail.com>
2022-09-27 01:49:07 +0900

    에러코드 추가

--- src/main/java/com/example/eunboard/exception/ErrorCode.java
+++ src/main/java/com/example/eunboard/exception/ErrorCode.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
+    TICKET_PASS_EXIST(401, "TICKET-PASS-ERR-401", "현재 티켓패스에 탑승중입니다."),
+    TICKET_PASS_NOT_FOUND(404, "TICKET-PASS-ERR-404", "티켓패스를 찾을 수 없습니다."),
     MEMBER_NOT_FOUND(404, "COMMON-ERR-404", "사용자를 찾을 수 없습니다"),
     MEMBER_NOT_AUTHORITY(500, "COMMON-ERR-500", "권한이 없습니다.");
 
